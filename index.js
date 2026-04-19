12dc92988dc15b4b765d1ca71c1f6a61ec19d47e
j-camposar <31163773+j-camposar@users.noreply.github.com>
2026-04-19 19:26:33 -0400

    eliminacion del nginx

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const BASE_PATH = "/api/v1/";
+const BASE_PATH = "http://localhost:8080/api/v1/";
 
 async function callApi(endpoint, method = 'GET', body = null) {
     const log = document.getElementById('responseLog');
